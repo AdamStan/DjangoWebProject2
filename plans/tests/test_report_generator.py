652fb251c25788bb3bd75811a170da672163d9b3
AdamStan <adam15stan@gmail.com>
2021-01-22 23:23:38 +0100

    adding performance tests for random algorithm

--- plans/tests/test_report_generator.py
+++ plans/tests/test_report_generator.py
@@ -1,9 +1,14 @@
 import unittest
 from plans.report_generator import BasicAlgorithmReport
+import os
 
 
 class TestAllParameters(unittest.TestCase):
     def test_report_generation(self):
         report_gen = BasicAlgorithmReport(time=100, result_value=123, quality_function_name="ddd",
                                           other_info_dict={"key1": "value1", "key2": "value2"})
-        report_gen.create_report()
+        self.file_name = report_gen.create_report()
+
+    def tearDown(self):
+        if self.file_name:
+            os.remove(self.file_name)
